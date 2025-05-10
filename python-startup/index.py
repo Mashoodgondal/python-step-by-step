277cf23e208ceae34328576ff9a5925168c8998d
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-05-10 22:18:50 +0500

    commit all methods

--- python-startup/index.py
+++ python-startup/index.py
@@ -48,27 +48,24 @@
 numbers = [10, 20, 30, 40, 50]
 
 # 1. Print all elements
-print("All numbers:", numbers)
+# print("All numbers:", numbers)
 
-# 2. Add an element to the list
-numbers.append(60)
-print("After appending 60:", numbers)
+# # 2. Add an element to the list
+# numbers.append(60)
+# print("After appending 60:", numbers)
 
-# 3. Remove an element
-numbers.remove(30)
-print("After removing 30:", numbers)
+# # 3. Remove an element
+# numbers.remove(30)
+# print("After removing 30:", numbers)
 
-# 4. Find the length of the list
-print("Length of the list:", len(numbers))
+# # 4. Find the length of the list
+# print("Length of the list:", len(numbers))
 
-# 5. Access a specific element
-print("First element:", numbers[0])
+# # 5. Access a specific element
+# print("First element:", numbers[0])
 
 # 6. Sum all elements
-print("Sum of all elements:", sum(numbers))
+# print("Sum of all elements:" , sum(numbers))
 
 # 7. Check if a number exists in the list
-if 20 in numbers:
-    print("20 is in the list")
-else:
-    print("20 is not in the list")
+
