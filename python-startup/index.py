3d47966d83278c2c95a83a5d20628d28b8f2e85a
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-05-10 21:52:01 +0500

    python practice

---
+++ python-startup/index.py
@@ -0,0 +1,28 @@
+# for i in range(5):
+#     print(i)
+
+# MARS_GRAVITY= 0.38
+# earth_weight=input("Enter earth weight in kg: ")
+# earth_weighted= float(earth_weight)
+# mars_weight= earth_weighted * MARS_GRAVITY
+# print(f"mars weight: {mars_weight}")
+
+# =========== Ratio on other planets ============
+
+
+# earth_weight = input("Enter Earth weight  ")
+# earth_weight= float(earth_weight)
+# gravity_ratios={
+#     "Mercury": 0.38,
+#         "Venus": 0.91,
+#         "Mars": 0.38,
+#         "Jupiter": 2.34,
+#         "Saturn": 1.06,
+#         "Uranus": 0.92,
+#         "Neptune": 1.19,
+#         "Pluto": 0.06
+# }
+# print("Weight of the object on other planets:")
+# for planet,ratio in gravity_ratios.items():
+#     planet_weight = earth_weight * ratio
+#     print(f"{planet}:{planet_weight:.3f}kg")
