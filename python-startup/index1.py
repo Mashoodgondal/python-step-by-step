f13b7ac3371a00a1e94670a5c3c0ec9f1fd9bd60
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-05-20 21:25:24 +0500

    string in python

---
+++ python-startup/index1.py
@@ -0,0 +1,9 @@
+# In Python, a string is a sequence of characters enclosed in quotes. You can use:
+
+# Single quotes 'hello'
+
+# Double quotes "hello"
+
+
+string = 'my name is mashood ali'
+print(string)
