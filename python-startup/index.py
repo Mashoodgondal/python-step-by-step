318328712bb08c8bb9c79dfa232d38f320d02a89
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-05-27 22:57:24 +0500

    remove duplicate from array

--- python-startup/index.py
+++ python-startup/index.py
@@ -217,6 +217,20 @@
 #         duplicate.append(i)
 # print(duplicate)
 
+# array= [2,5,6,7,6,5,4,3,2]
+# duplicate =[]
+# for i in array:
+#     if i not in duplicate:
+#         duplicate.append(i)
+# print(duplicate)
+
+# array= [2,5,6,7,6,5,4,3,2]
+# duplicate =[]
+# for i in array:
+#     if i not in duplicate:
+#         duplicate.append(i)
+# print(duplicate)
+
 
 
 
