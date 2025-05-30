8968986f623709882b75a5140b98c33463083f05
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-05-30 21:55:27 +0500

    string in adwance

--- python-startup/index1.py
+++ python-startup/index1.py
@@ -201,4 +201,31 @@
 #     unique =list(set(arr))
 #     unique.sort(reverse=True)
 #     return unique[2]
+# arr = [2,4,6,7,8,6,8,9,6]
+# def second_largest(arr):
+#     unique =list(set(arr))
+#     unique.sort(reverse=True)
+#     return unique[1]
+# print(second_largest(arr))
+
+# ============third largest ===============
+
+# arr = [2,4,6,7,8,6,8,9,6]
+# def second_largest(arr):
+#     unique =list(set(arr))
+#     unique.sort(reverse=True)
+#     return unique[2]# arr = [2,4,6,7,8,6,8,9,6]
+# def second_largest(arr):
+#     unique =list(set(arr))
+#     unique.sort(reverse=True)
+#     return unique[1]
+# print(second_largest(arr))
+
+# ============third largest ===============
+
+# arr = [2,4,6,7,8,6,8,9,6]
+# def second_largest(arr):
+#     unique =list(set(arr))
+#     unique.sort(reverse=True)
+#     return unique[2]
 
