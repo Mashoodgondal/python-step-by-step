ff918788708d7194fb635235e64102d6bc696550
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-07-09 22:08:20 +0500

    commit

--- python-startup/index1.py
+++ python-startup/index1.py
@@ -189,3 +189,28 @@ print(second_largest(arr))
 # print(second)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ====================================
+# string = str(input("Enter a string: "))
+# second= ''
+# for char in string:
+#     if char not in second:
+#         second +=char
+     
+# print(second)
