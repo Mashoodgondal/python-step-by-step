22df68139ba7ad113edb6599c0ff917c53c9a128
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-08-14 22:16:42 +0500

    revise previous functions

--- python-startup/index1.py
+++ python-startup/index1.py
@@ -204,3 +204,16 @@
 #     unique.sort(reverse=True)
 #     return unique[2]
 # print(second_largest(arr)
+
+
+# arr = [2,4,6,7,8,6,8,9,6]
+# def second_largest(arr):
+#     unique =list(set(arr))
+#     unique.sort(reverse=True)
+#     return unique[2]
+# print(second_largest(arr)# arr = [2,4,6,7,8,6,8,9,6]
+# def second_largest(arr):
+#     unique =list(set(arr))
+#     unique.sort(reverse=True)
+#     return unique[2]
+# print(second_largest(arr)
